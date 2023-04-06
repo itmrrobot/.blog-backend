f0e60de67f48a60d689777958cc201fc76601860
itmrrobot <nhannguyentri05@gmail.com>
2023-04-06 21:27:34 +0700

    Add some code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGODB_URL);
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true"
+},() => {
+    console.log("Success")
+});
