3dd13531d2d71a4d5f4a9bb7d25dc4b1d1183dd2
itmrrobot <nhannguyentri05@gmail.com>
2023-04-07 10:35:26 +0700

    Modify some code

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 require('./db/mongoose');
 const blogRouter = require('./router/blogs');
 const userRouter = require('./router/user');
+const path = require('path');
+const imagesPath = path.join(__dirname,'../images');
 const app = express();
 
-app.use('/images',express.static('images'));
+app.use(express.static(imagesPath));
 app.use(express.json());
 app.use(blogRouter);
 app.use(userRouter);
-
 
 app.listen(process.env.PORT,() => {
     console.log('Server started on port 4000');
